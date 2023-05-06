47c1a4f2faad66d5e40d4cb7bfe8de84f5632e01
Microsoft Learn Student <learn@contoso.com>
2023-05-06 19:16:59 +0200

    black

--- Password Manager/app/main.py
+++ Password Manager/app/main.py
@@ -3,14 +3,14 @@ from app.password_logic import load_passwords, save_passwords, add_password
 
 
 def main():
-    file_path = os.path.join(os.path.dirname(__file__), '..', 'passwords.json')
+    file_path = os.path.join(os.path.dirname(__file__), "..", "passwords.json")
     passwords = load_passwords(file_path)
 
     new_entry = {
-        'name': 'test',
-        'url': 'https://www.test.com',
-        'username': 'test@example.com',
-        'password': 'testp@ss'
+        "name": "test",
+        "url": "https://www.test.com",
+        "username": "test@example.com",
+        "password": "testp@ss",
     }
 
     updated_passwords = add_password(passwords, **new_entry)
@@ -18,5 +18,5 @@ def main():
     print("New entry added successfully!")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- Password Manager/app/password_logic.py
+++ Password Manager/app/password_logic.py
@@ -2,21 +2,18 @@ import json
 
 
 def load_passwords(file_path):
-    with open(file_path, 'r') as file:
+    with open(file_path, "r") as file:
         passwords = json.load(file)
     return passwords
 
 
 def save_passwords(file_path, passwords):
-    with open(file_path, 'w') as file:
+    with open(file_path, "w") as file:
         json.dump(passwords, file)
 
 
 def add_password(passwords, name, url, username, password):
-    passwords['passwords'].append({
-        'name': name,
-        'url': url,
-        'username': username,
-        'password': password
-    })
+    passwords["passwords"].append(
+        {"name": name, "url": url, "username": username, "password": password}
+    )
     return passwords
